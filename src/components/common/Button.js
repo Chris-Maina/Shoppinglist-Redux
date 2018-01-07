28d28714033a89b9e9fa31df79190ccb601e6cd1
Chris-Maina <krismaish34@gmail.com>
2018-01-07 23:30:26 +0300

    [feature #154115541] Refactor button props.

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -3,7 +3,11 @@ import { Button } from 'semantic-ui-react';
 
 const CustButton = (props) =>{
     return(
-        <Button fluid={props.fluid} size={props.size} color={props.color}>{props.buttonName}</Button>
+        <Button 
+        fluid={props.fluid} 
+        size={props.size} 
+        color={props.color}
+        >{props.buttonName}</Button>
     );
 }
 export default CustButton;
