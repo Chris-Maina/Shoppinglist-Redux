4e639d0a1a008667347b99481bc7b04e4df2f9cd
Chris-Maina <krismaish34@gmail.com>
2018-01-08 21:10:35 +0300

    [feature #154115541] Refactor action creator names.

--- src/actions/shoppinglistActions.js
+++ src/actions/shoppinglistActions.js
@@ -2,21 +2,21 @@ import * as types from './actionConst'
 import axiosConfig from '../components/common/baseConfig';
 import { toast } from 'react-toastify';
 
-export function shoppinglistRequest() {
+export function getshoppinglistRequest() {
     return { type: types.GET_SHOPPINGLIST_REQUEST }
 
 }
-export function shoppinglistSuccess(response) {
+export function getshoppinglistSuccess(response) {
     return { type: types.GET_SHOPPINGLIST_SUCCESS, response }
 }
-export function shoppinglistFail(error) {
+export function getshoppinglistFail(error) {
     return { type: types.GET_SHOPPINGLIST_FAIL, error }
 }
 
 export function getShoppinglist() {
     return function (dispatch) {
         // dispatch a get request
-        dispatch(shoppinglistRequest());
+        dispatch(getshoppinglistRequest());
         return axiosConfig.request({
             method: 'get',
             url: '/shoppinglists/',
@@ -25,15 +25,15 @@ export function getShoppinglist() {
             }
         }).then(response => {
             // dispatch a get success
-            dispatch(shoppinglistSuccess(response))
+            dispatch(getshoppinglistSuccess(response))
         }).catch(error => {
             if (error.response) {
                 // The request was made and the server responded with a status code
             // that falls out of the range of 2xx
             toast.error(error.response.data.message);
-            
+
             // dispatch a get error
-            dispatch(shoppinglistFail(error))
+            dispatch(getshoppinglistFail(error))
             }
             
         })
