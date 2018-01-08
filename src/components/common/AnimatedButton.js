d7344cdc77228889ac992f0b00e0bb91ef9bc9bd
Chris-Maina <krismaish34@gmail.com>
2018-01-08 21:00:45 +0300

    [feature #154115541] Remove the circular props.

--- src/components/common/AnimatedButton.js
+++ src/components/common/AnimatedButton.js
@@ -3,7 +3,7 @@ import { Button, Icon } from 'semantic-ui-react';
 
 const AnimatedButton = (props) => {
     return(
-        <Button animated floated={props.floated} size={props.size} circular={props.circular} color={props.color}>
+        <Button animated floated={props.floated} size={props.size} color={props.color}>
         <Button.Content visible> {props.content}</Button.Content>
         <Button.Content hidden> 
         <Icon name={props.iconName}/></Button.Content>
