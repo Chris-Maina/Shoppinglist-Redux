92550e62a45502d7389707498b53275bf56ade71
Chris-Maina <krismaish34@gmail.com>
2018-01-09 16:29:30 +0300

    [feature #154081230] Add reducer function for shopping list actions.

---
+++ src/reducers/shoppinglistReducer.js
@@ -0,0 +1,38 @@
+import * as actionTypes from '../actions/actionConst';
+import * as initialState from './initialState';
+
+export default function shoppinglistReducer(state = initialState, action) {
+    switch (action.type) {
+        case actionTypes.GET_SHOPPINGLIST_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                redirect: false
+            }
+        case actionTypes.GET_SHOPPINGLIST_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                redirect: false,
+                shoppinglists: action.response.data.shopping_lists
+            }
+        case actionTypes.GET_SHOPPINGLIST_FAIL:
+            return {
+                ...state,
+                loading: false,
+                redirect: false
+            }
+        case actionTypes.TOGGLE_FORM_OPEN:
+            return {
+                ...state,
+                isFormOpen: true
+            }
+        case actionTypes.CREATE_SHOPPINGLIST_REQUEST:
+            return {
+                ...state,
+                isFormOpen: false
+            }
+        default:
+            return state;
+    }
+}
