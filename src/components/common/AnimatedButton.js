e9a1c43071f60e7f5d45a87c0e5698a552293bac
Chris-Maina <krismaish34@gmail.com>
2018-01-09 17:20:31 +0300

    [feature #154081230] Add onClick prop.

--- src/components/common/AnimatedButton.js
+++ src/components/common/AnimatedButton.js
@@ -3,7 +3,7 @@ import { Button, Icon } from 'semantic-ui-react';
 
 const AnimatedButton = (props) => {
     return(
-        <Button animated floated={props.floated} size={props.size} color={props.color}>
+        <Button animated floated={props.floated} size={props.size} color={props.color} onClick={props.onClick}>
         <Button.Content visible> {props.content}</Button.Content>
         <Button.Content hidden> 
         <Icon name={props.iconName}/></Button.Content>
