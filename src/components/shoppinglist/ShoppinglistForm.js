7425b327dd74b291444d2ca5d02e4fa4c0a0eea3
Chris-Maina <krismaish34@gmail.com>
2018-01-14 17:08:14 +0300

    [feature #154081234] Refactor to use stateless ShoppinglistForm component.

--- src/components/shoppinglist/ShoppinglistForm.js
+++ src/components/shoppinglist/ShoppinglistForm.js
@@ -1,40 +1,26 @@
-import React, {Component} from 'react'
+import React from 'react'
 import { Form, Button } from 'semantic-ui-react'
-import { connect } from 'react-redux'; 
-import CustFormInput from '../common/Input';
-import PropTypes from 'prop-types';
 
-class ShoppinglistForm extends Component{
-    constructor(props){
-        super(props);
-    }
-    render(){
-        const button_text_name = this.props.value ? 'Update' : 'Create'
+import CustFormInput from '../common/Input';
+
+const ShoppinglistForm = (props)=> {
+        const button_text_name = props.value ? 'Update' : 'Create'
         return (
-            <Form onSubmit={this.props.onSubmit}>
+            <Form onSubmit={props.onSubmit}>
                 <Form.Group >
                     <CustFormInput
-                        width={this.props.width}
-                        type={this.props.type}
-                        placeholder={this.props.placeholder}
-                        name={this.props.name}
-                        value={this.props.value}
-                        onChange={this.props.onChange}
+                        width={props.width}
+                        type={props.type}
+                        placeholder={props.placeholder}
+                        name={props.name}
+                        value={props.value}
+                        onChange={props.onChange}
                     />
                     <Button size="large" color="blue" content={button_text_name} />
-                    <Button size="large" color="red" content="Cancel" onClick={this.props.onCancelClick}/>
+                    <Button size="large" color="red" content="Cancel" onClick={props.onCancelClick}/>
                 </Form.Group>
             </Form>
         );
-    }
 }
-ShoppinglistForm.propTypes = {
-    params: PropTypes.object
 
-}
-function mapStateToProps(state, ownProps) {
-    return {
-        state: state
-    }
-}
-export default connect(mapStateToProps)(ShoppinglistForm);
+export default ShoppinglistForm;
