9f6b3167107fea349d200351c8d25b0b724d9397
Chris-Maina <krismaish34@gmail.com>
2018-01-15 20:58:30 +0300

    [feature #154329764] Add map function to map shopping items in props.

--- src/components/shoppingitems/ShoppingItemsPage.js
+++ src/components/shoppingitems/ShoppingItemsPage.js
@@ -64,48 +64,30 @@ class ShoppingItemsPage extends Component {
                                             </Table.Row>
                                         </Table.Header>
                                         <Table.Body>
-                                            <Table.Row>
-                                                <Table.Cell>Eggs</Table.Cell>
-                                                <Table.Cell>2</Table.Cell>
-                                                <Table.Cell>24</Table.Cell>
-                                                <Table.Cell>
-                                                    <CustButton
-                                                        size="tiny"
-                                                        color="green"
-                                                        icon="edit"
-                                                        circular
-                                                    />
-                                                </Table.Cell>
-                                                <Table.Cell>
-                                                    <CustButton
-                                                        size="tiny"
-                                                        color="red"
-                                                        icon="trash"
-                                                        circular
-                                                    />
-                                                </Table.Cell>
-                                            </Table.Row>
-                                            <Table.Row>
-                                                <Table.Cell>Milk</Table.Cell>
-                                                <Table.Cell>1</Table.Cell>
-                                                <Table.Cell>50</Table.Cell>
-                                                <Table.Cell>
-                                                    <CustButton
-                                                        size="tiny"
-                                                        color="green"
-                                                        icon="edit"
-                                                        circular
-                                                    />
-                                                </Table.Cell>
-                                                <Table.Cell>
-                                                    <CustButton
-                                                        size="tiny"
-                                                        color="red"
-                                                        icon="trash"
-                                                        circular
-                                                    />
-                                                </Table.Cell>
-                                            </Table.Row>
+                                            {this.props.shoppingitems.map( oneshoppingitem => 
+                                                <Table.Row
+                                                    key={oneshoppingitem.id}>
+                                                    <Table.Cell>{oneshoppingitem.name}</Table.Cell>
+                                                    <Table.Cell>{oneshoppingitem.quantity}</Table.Cell>
+                                                    <Table.Cell>{oneshoppingitem.price}</Table.Cell>
+                                                    <Table.Cell>
+                                                        <CustButton
+                                                            size="tiny"
+                                                            color="green"
+                                                            icon="edit"
+                                                            circular
+                                                        />
+                                                    </Table.Cell>
+                                                    <Table.Cell>
+                                                        <CustButton
+                                                            size="tiny"
+                                                            color="red"
+                                                            icon="trash"
+                                                            circular
+                                                        />
+                                                    </Table.Cell>
+                                                </Table.Row>
+                                            )}
                                         </Table.Body>
 
                                     </Table>
