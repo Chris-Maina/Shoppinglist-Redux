37e9c0cdd4134ad9f6b99e58fa20bb832571235c
Chris-Maina <krismaish34@gmail.com>
2018-01-16 13:46:49 +0300

    [Feature #154081230] Add create/edit shopping item form component.

---
+++ src/components/shoppingitems/ShoppingItemForm.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Form, Button } from 'semantic-ui-react'
+import CustFormInput from '../common/Input';
+
+
+const ShoppingItemForm = props => {
+        let button_text_name = props.isEditClicked ? 'Update' : 'Create';
+        return (
+            <Form onSubmit={props.onSubmit}>
+                <Form.Group >
+                    <CustFormInput
+                        width={props.width}
+                        type="text"
+                        placeholder="Shopping item name"
+                        name="shoppingitemname"
+                        value={props.item.shoppingitemname}
+                        onChange={props.onChange}
+                    />
+                    <CustFormInput
+                        width={props.width}
+                        type="number"
+                        placeholder="Quantity"
+                        name="quantity"
+                        value={props.item.quantity}
+                        onChange={props.onChange}
+                    />
+                    <CustFormInput
+                        width={props.width}
+                        type="number"
+                        placeholder="Price"
+                        name="price"
+                        value={props.item.price}
+                        onChange={props.onChange}
+                    />
+                    <Button size="large" color="blue" content={button_text_name} />
+                    <Button size="large" color="red" content="Cancel" onClick={props.onCancelClick}/>
+                </Form.Group>
+            </Form>
+        );
+}
+
+export default ShoppingItemForm;
