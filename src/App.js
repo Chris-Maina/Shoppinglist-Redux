fdf30e98a98a43808672d42519e85e0940a8440c
Chris-Maina <krismaish34@gmail.com>
2018-01-18 14:35:46 +0300

    [154389084] Refactor path of shoppinglists.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from './components/home/HomePage';
 import RegisterPage from './components/register/RegisterPage';
 import LoginPage from './components/login/LoginPage';
 import ShoppinglistPage from './components/shoppinglist/ShoppinglistPage';
-import ShoppinglistForm from './components/shoppinglist/ShoppinglistForm';
 import ShoppingItemsPage from './components/shoppingitems/ShoppingItemsPage';
 
 class App extends Component {
@@ -19,9 +18,8 @@ class App extends Component {
         <Route exact={true} path="/" component={Home}/>
         <Route exact={true} path="/auth/register/" component={RegisterPage} />
         <Route exact={true} path="/auth/login/" component={LoginPage} />
-        <Route exact={true} path="/shoppinglist" component={ShoppinglistPage}/>
-        <Route exact={true} path={`/shoppinglist/:id`} component={ShoppinglistForm}/>
-        <Route exact={true} path="/shoppinglist/:id/items" component={ShoppingItemsPage}/>
+        <Route exact={true} path="/shoppinglists/" component={ShoppinglistPage}/>
+        <Route exact={true} path="/shoppinglists/:id/items" component={ShoppingItemsPage}/>
         </Switch>
       </div>
     );
